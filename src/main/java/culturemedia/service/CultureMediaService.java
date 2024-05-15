f445cb5ffd1be27b67516e824dbf9e4cf56ae721
SantiagoAvilaa <s.avila@udea.edu.co>
2024-05-15 12:36:45 -0500

    add excep

--- src/main/java/culturemedia/service/CultureMediaService.java
+++ src/main/java/culturemedia/service/CultureMediaService.java
@@ -8,6 +8,8 @@ import culturemedia.model.View;
 
 public interface CultureMediaService {
     List<Video> findAllVideos() throws VideoNotFoundException;
+    List<Video> find(String title) throws VideoNotFoundException;
+    List<Video> find(double fromDuration, double toDuration) throws VideoNotFoundException;
     Video add(Video video);
     View add(View view);
 }
